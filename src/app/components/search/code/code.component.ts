4d0fe95ba0107f3a3d21930cee761fbc0ddb1da2
Thiago <thiagoestevao3030@gmail.com>
2025-08-02 02:54:33 -0300

    rollback

---
+++ src/app/components/search/code/code.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code',
+  imports: [],
+  templateUrl: './code.component.html',
+  styleUrl: './code.component.scss'
+})
+export class CodeComponent {
+
+}
